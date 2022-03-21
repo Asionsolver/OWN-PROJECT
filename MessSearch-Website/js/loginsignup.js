f357cde182d9f1a46b033603f8c0aca7ba7b24da
Asionsolver <asionsolver274@gmail.com>
2022-03-21 22:29:40 +0600

    project complete name:signup page

---
+++ MessSearch-Website/js/loginsignup.js
@@ -0,0 +1,13 @@
+const sign_in_btn = document.querySelector('#sign-up-button');
+const sign_up_btn = document.querySelector('#sign-in-button');
+const container = document.querySelector('.container');
+
+// console.log(container);
+// console.log(sign_in_btn);
+
+sign_up_btn.addEventListener('click',()=>{
+    container.classList.add('sign-up-mode');
+});
+sign_in_btn.addEventListener('click',()=>{
+    container.classList.remove('sign-up-mode');
+});
