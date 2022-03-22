9d59b74c6e8fd7db3c32f1a58663f431dc432341
Asionsolver <asionsolver274@gmail.com>
2022-03-22 08:51:49 +0600

    add file like loginpage.html

--- MessSearch-Website/js/loginsignup.js
+++ MessSearch-Website/js/loginsignup.js
@@ -1,13 +1,11 @@
-const sign_in_btn = document.querySelector('#sign-up-button');
-const sign_up_btn = document.querySelector('#sign-in-button');
-const container = document.querySelector('.container');
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-// console.log(container);
-// console.log(sign_in_btn);
-
-sign_up_btn.addEventListener('click',()=>{
-    container.classList.add('sign-up-mode');
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
 });
-sign_in_btn.addEventListener('click',()=>{
-    container.classList.remove('sign-up-mode');
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
 });
