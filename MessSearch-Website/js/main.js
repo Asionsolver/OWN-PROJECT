6e8f2e8fcdfc7a80febe61917eb5fa201bb56a2f
Asionsolver <asionsolver274@gmail.com>
2022-03-22 12:25:08 +0600

    Modified MessSearch Website

--- MessSearch-Website/js/main.js
+++ MessSearch-Website/js/main.js
@@ -8,3 +8,12 @@ window.onscroll = function(){
         nav.classList.remove("header-scrolled");
     }
 }
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
