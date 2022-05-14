c317b9e4e39a0a2bf5b9e212b28d2fca35d51ea2
Asionsolver <asionsolver274@gmail.com>
2022-05-15 01:47:06 +0600

    Modified realState Website and 90% work complete

--- RealStateWebsite/js/main.js
+++ RealStateWebsite/js/main.js
@@ -8,3 +8,70 @@ function scrollHeader(){
     // else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+// **============================= Swiper POPULAR ============================= **//
+
+const swiperPopular = new Swiper('.popular-container', {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev', 
+    },
+});
+
+// **============================= VALUE ACCORDION ============================= **//
+
+const accordionItems = document.querySelectorAll('.value-accordion-item')
+
+accordionItems.forEach(item => {
+    const accordionHeader = item.querySelector('.value-accordion-header')
+
+
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+        toggleItem(item)
+        if(openItem && openItem !== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.value-accordion-content')
+
+    if(item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+
+    // accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    // item.classlist.add('accordion-open')
+}
+
+//**=============== SCROLL SECTIONS ACTIVE LINK ===============**/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
